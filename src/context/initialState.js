a14a39cc1aac7c7b765b3c9e2a3d80b2b7b873ad
fernandogutz <03fernandogutierrez@gmail.com>
2023-03-07 17:37:06 -0300

    context folder deleted

--- src/context/initialState.js
+++
@@ -1,8 +0,0 @@
-import useFetch from "@/hooks/useFetch";
-
-export const initialState = async () => {
-    const posts = await useFetch('https://blog.fernandogutz.com/wp-json/wp/v2/posts/');
-
-    return posts;
-
-}
