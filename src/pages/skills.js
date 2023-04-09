62de56db62fb643e4d22b539cb417939397beed1
fernandogutz <03fernandogutierrez@gmail.com>
2023-04-08 23:54:15 -0400

    Menu updated

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,6 +1,7 @@
 import Skills from '@/components/skills/Skills'
 import Footer from '@/components/ui/Footer'
 import Header from '@/components/ui/Header'
+import { setActiveMenuLink } from '@/functions/setActiveMenuLink'
 import React from 'react'
 
 const SkillsPage = ({ skills }) => {
