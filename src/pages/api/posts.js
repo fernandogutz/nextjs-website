f60f98b39dfaed7ad09872d4162080339b73f29b
fernandogutz <03fernandogutierrez@gmail.com>
2023-04-09 00:27:47 -0400

    API Limit per page updated

--- src/pages/api/posts.js
+++ src/pages/api/posts.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    const response = await fetch("https://blog.fernandogutz.com/wp-json/wp/v2/posts/");
+    const response = await fetch("https://blog.fernandogutz.com/wp-json/wp/v2/posts?per_page=100");
     const data = await response.json();
     res.status(200).json(data);
   }
--- src/pages/api/projects.js
+++ src/pages/api/projects.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    const response = await fetch("https://blog.fernandogutz.com/wp-json/wp/v2/project/");
+    const response = await fetch("https://blog.fernandogutz.com/wp-json/wp/v2/project?per_page=100");
     const projects = await response.json();
 
     res.status(200).json(projects)
